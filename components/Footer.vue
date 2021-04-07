bc9ff8566efd418ed14bb7e26833eb3c2fe7f9fc
Charles Veneau <charles.veneau@gmail.com>
2021-04-07 17:17:41 +0200

    Create global footer

---
+++ components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="footer">
+    <div class="container row">
+      <p class="footer-text">Keep in touch ?</p>
+      <ul class="footer-list">
+        <li class="footer-listElt">
+          <a href="mailto:charlesveneau@gmail.com">charles.veneau@gmail.com</a>
+        </li>
+        <li class="footer-listElt">
+          <a href="https://www.linkedin.com/in/charles-veneau-08342895/"
+            >LinkedIn</a
+          >
+        </li>
+        <li class="footer-listElt">
+          <a href="https://github.com/charlesVeneau">Github</a>
+        </li>
+        <li class="footer-listElt">
+          <a href="tel:+33770042261">+33 (0)7 70 04 22 61</a>
+        </li>
+      </ul>
+    </div>
+  </footer>
+</template>
