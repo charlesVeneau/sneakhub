59f8698453014501a75c7a1231ffeceedc08bea1
Charles Veneau <charles.veneau@gmail.com>
2021-04-24 12:07:15 +0200

    Add icon system Implement icons on product card

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="item-card dots">
+    <div class="product-icons">
+      <div class="icon">
+        <icon-base
+          icon-name="email"
+          icon-color="#000000"
+          width="12"
+          height="12"
+        >
+          <icon-zoom />
+        </icon-base>
+      </div>
+      <div class="icon">
+        <icon-base
+          icon-name="email"
+          icon-color="#000000"
+          width="12"
+          height="12"
+        >
+          <icon-cart />
+        </icon-base>
+      </div>
+    </div>
     <img :src="item.imgs[0]" alt="" class="productCard-img" />
     <h4 class="productCard-title">{{ item.brand }} {{ item.name }}</h4>
     <p class="productCard-text">${{ item.price }}</p>
@@ -8,8 +30,16 @@
 </template>
 
 <script>
+import IconBase from '@/components/IconBase.vue'
+import IconZoom from '@/components/icons/IconZoom.vue'
+import IconCart from '@/components/icons/IconCart.vue'
 export default {
   name: 'ProductCard',
+  components: {
+    IconBase,
+    IconZoom,
+    IconCart,
+  },
   props: {
     item: {
       type: Object,
